018c1d2b5702ca48336e624e0d04d91c31a98710
Tom <tom@holyticket.com>
2016-06-29 22:32:13 +0200

    Tests vergeten toe te voegen, nu alsnog gedaan

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -9,9 +9,40 @@ class ConnectionTest extends TestCase
     public function testConnectToDatabase()
     {
         // Checkt of er verbinding kan worden gemaakt met een database
+        $db = [
+            'host' => '127.0.0.1',
+            'database_name' => 'ht',
+            'username' => 'root',
+            'password' => 'root'
+        ];
+
+        $connection_result = Connect::connect('test', $db);
+        $this->assertTrue($connection_result);
+
     }
     public function testResetDatabase()
     {
-        // Checkt of de verbinding juist wordt gereset (teruggezet naar orginele databae)
+        // Checkt of de verbinding juist wordt gereset (teruggezet naar orginele database)
+        Connect::reset();
+        $current_db = Config::get('database.default');
+        $this->assertEquals('mysql', $current_db);
+    }
+    public function testGetAdditionalDatabases() {
+
+        $this->connectionMock = Mockery::mock('alias:App\Models\Connection');
+
+        $this->connectionMock->shouldReceive('getAttribute')->withAnyArgs();
+        $this->connectionMock->shouldReceive('setAttribute')->withAnyArgs();
+
+        $this->connectionMock->host = '127.0.0.1';
+        $this->connectionMock->username = 'root';
+        $this->connectionMock->password = 'root';
+        $this->connectionMock->database_name = 'ht';
+
+        $this->app->instance(App\Models\Connection::class, $this->connectionMock);
+
+        $databases = Connect::getOtherDatabases($this->connectionMock);
+
+        $this->assertNotEmpty($databases);
     }
 }
--- tests/SqlGenerationTest.php
+++ tests/SqlGenerationTest.php
@@ -9,23 +9,154 @@ class SqlGenerationTest extends TestCase
     private $sqlService;
 
     public function __construct() {
-        $this->sqlService = new \App\Services\SqlGenerationService();
+
     }
     public function testCreateTable_ShouldReturn_ValidSQL()
     {
         // Test of de CREATE TABLE queries correct zijn.
+        $table = Mockery::mock('alias:App\Models\Virtual\Table');
+
+
+        $table->shouldReceive('addColumn')->withAnyArgs();
+        $table->shouldReceive('getName')->andReturn('hallo');
+        $table->shouldReceive('getCollation')->andReturn('myisam');
+        $table->shouldReceive('getRowFormat')->andReturn('fixed');
+        $table->shouldReceive('getEngine')->andReturn('innodb');
+
+        // Add a column
+            $column = Mockery::mock('alias:App\Models\Virtual\Column');
+            $column->shouldReceive('getAutoIncrement')->withNoArgs();
+            $column->name = 'test';
+            $column->type = 'INT(11)';
+            $column->notnull = true;
+            $column->default = 'hoi';
+            $column->auto_increment = false;
+            $column->comment = '';
+
+            $column->shouldReceive('getAttributes')->andReturn((array) get_object_vars($column));
+
+            $columns = [];
+            $columns[] = $column;
+
+            $table->shouldReceive('getColumns')->andReturn($columns);
+
+        // Add a index
+            $index = Mockery::mock('alias:App\Models\Virtual\Index');
+            $index->shouldReceive('getPrimary', 'getUnique')->andReturn(false);
+            $index->shouldReceive('getName')->andReturn('test_index');
+
+            $index->name = 'test_index';
+            $index->columns = [];
+            $index->unique = false;
+            $index->primary = false;
+
+            $index->shouldReceive('getColumns')->withNoArgs()->andReturn([]);
+
+            $indexes = [];
+            $indexes[] = $index;
+
+            $table->shouldReceive('getIndices')->andReturn($indexes);
+
+        $this->app->instance(App\Models\Virtual\Column::class, $column);
+        $this->app->instance(App\Models\Virtual\Table::class, $table);
+
+        $sql_expected = "CREATE TABLE IF NOT EXISTS `hallo` (
+`test` INT(11) NOT NULL   DEFAULT 'hoi' ,
+KEY `test_index` (``)
+) CHARACTER SET myisam COLLATE myisam ROW_FORMAT=fixed  ENGINE=innodb;
+
+
+
+";
+
+        $this->assertEquals($sql_expected, Sql::createTable($table));
     }
     public function testDropTable_ValidSQL()
     {
         // Test of de CREATE TABLE queries correct zijn.
+        $table = Mockery::mock('alias:App\Models\Virtual\Table');
+        $table->shouldReceive('getName')->andReturn('table_name');
+
+        $sql_expected = 'DROP TABLE `table_name`;
+';
+
+        $this->assertEquals($sql_expected, Sql::dropTable($table));
+
     }
     public function testaddColumn_ValidSQL() {
         // Test of de ADD COLUMN queries correct zijn.
+
+        // Create a table
+        $table = Mockery::mock('alias:App\Models\Virtual\Table');
+        $table->shouldReceive('getName')->andReturn('table_name');
+
+        // Create a column
+        $column = Mockery::mock('alias:App\Models\Virtual\Column');
+        $column->shouldReceive('getAutoIncrement')->withNoArgs();
+        $column->name = 'test';
+        $column->type = 'INT(11)';
+        $column->notnull = true;
+        $column->default = 'hoi';
+        $column->auto_increment = false;
+        $column->comment = '';
+
+        $column->shouldReceive('getAttributes')->andReturn((array) get_object_vars($column));
+
+        $expected_sql = "ALTER TABLE `table_name` ADD `test` INT(11) NOT NULL   DEFAULT 'hoi'  ;
+";
+        $this->assertEquals($expected_sql, Sql::addColumn($table, $column));
     }
     public function testAlterColumn_ValidSQL() {
         // Test of de ALTER COLUMN queries correct zijn.
+
+        // Create a column
+        $column = Mockery::mock('alias:App\Models\Virtual\Column');
+        $column->shouldReceive('getAutoIncrement')->withNoArgs();
+        $column->name = 'test';
+        $column->type = 'INT(11)';
+        $column->notnull = true;
+        $column->default = 'hoi';
+        $column->auto_increment = false;
+        $column->comment = '';
+
+        $column->shouldReceive('getAttributes')->andReturn((array) get_object_vars($column));
+
+        $expected_sql = "ALTER TABLE `table_name` CHANGE `test` `test` INT(11) NOT NULL   DEFAULT 'hoi' ; \n";
+
+        $attributes = (array) get_object_vars($column);
+
+        $this->assertEquals($expected_sql, Sql::alterColumn($column, $attributes, 'table_name'));
     }
     public function testAddIndex_ValidSQL() {
         // Test of de ADD INDEX queries correct zijn. Hieronder vallen de queries voor de primary keys, unique keys en reguliere keys.
+
+        // Test of de CREATE TABLE queries correct zijn.
+        $table = Mockery::mock('alias:App\Models\Virtual\Table');
+
+
+        $table->shouldReceive('addColumn')->withAnyArgs();
+        $table->shouldReceive('getName')->andReturn('hallo');
+        $table->shouldReceive('getCollation')->andReturn('myisam');
+        $table->shouldReceive('getRowFormat')->andReturn('fixed');
+        $table->shouldReceive('getEngine')->andReturn('innodb');
+
+        // Add a index
+        $index = Mockery::mock('alias:App\Models\Virtual\Index');
+        $index->shouldReceive('getPrimary', 'getUnique')->andReturn(false);
+        $index->shouldReceive('getName')->andReturn('test_index');
+
+        $index->name = 'test_index';
+        $index->columns = [];
+        $index->unique = false;
+        $index->primary = false;
+
+        $index->shouldReceive('getColumns')->withNoArgs()->andReturn([]);
+
+
+        $expected_sql = "ALTER TABLE `hallo` ADD KEY `test_index` (``);
+";
+
+        $this->assertEquals($expected_sql, Sql::addIndex($table, $index));
+
     }
 }
